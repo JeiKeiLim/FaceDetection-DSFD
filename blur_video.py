194b395b239ecc9f3fc4b4982113a2a05d1bd47d
Jongkuk Lim <lim.jeikei@gmail.com>
2019-12-31 11:56:51 +0900

    Cleaned unused code. Removed test code.

--- blur_video.py
+++ blur_video.py
@@ -1,24 +1,11 @@
 from __future__ import print_function
-import sys
-import os
 import argparse
 import torch
-import torch.nn as nn
-import torch.backends.cudnn as cudnn
-import torchvision.transforms as transforms
 from torch.autograd import Variable
-from data import WIDERFace_ROOT, WIDERFace_CLASSES as labelmap
-from PIL import Image
-from data import WIDERFaceDetection, WIDERFaceAnnotationTransform, WIDERFace_CLASSES, WIDERFace_ROOT, BaseTransform, \
-    TestBaseTransform
 from data import *
-import torch.utils.data as data
 from face_ssd import build_ssd
-# from resnet50_ssd import build_sfd
-import pdb
 import numpy as np
 import cv2
-import math
 import matplotlib.pyplot as plt
 import time
 
@@ -44,8 +31,7 @@ parser.add_argument('--trained_model', default='weights/WIDERFace_DSFD_RES152.pt
 parser.add_argument('--threshold', default=0.1, type=float,
                     help='Final confidence threshold')
 parser.add_argument('--cuda', default=True, type=bool,
-                    help='Use cuda to train model')
-parser.add_argument('--widerface_root', default=WIDERFace_ROOT, help='Location of WIDERFACE root directory')
+                    help='Use cuda')
 args = parser.parse_args()
 
 if args.cuda and torch.cuda.is_available():
@@ -54,49 +40,6 @@ else:
     torch.set_default_tensor_type('torch.FloatTensor')
 
 torch.set_grad_enabled(False)
-
-
-def bbox_vote(det):
-    order = det[:, 4].ravel().argsort()[::-1]
-    det = det[order, :]
-    while det.shape[0] > 0:
-        # IOU
-        area = (det[:, 2] - det[:, 0] + 1) * (det[:, 3] - det[:, 1] + 1)
-        xx1 = np.maximum(det[0, 0], det[:, 0])
-        yy1 = np.maximum(det[0, 1], det[:, 1])
-        xx2 = np.minimum(det[0, 2], det[:, 2])
-        yy2 = np.minimum(det[0, 3], det[:, 3])
-        w = np.maximum(0.0, xx2 - xx1 + 1)
-        h = np.maximum(0.0, yy2 - yy1 + 1)
-        inter = w * h
-        o = inter / (area[0] + area[:] - inter)
-        # get needed merge det and delete these det
-        merge_index = np.where(o >= 0.3)[0]
-        det_accu = det[merge_index, :]
-        det = np.delete(det, merge_index, 0)
-        if merge_index.shape[0] <= 1:
-            continue
-
-        det_accu[:, 0:4] = det_accu[:, 0:4] * np.tile(det_accu[:, -1:], (1, 4))
-        max_score = np.max(det_accu[:, 4])
-
-        if type(max_score) == torch.Tensor:
-            max_score = max_score.cpu().numpy()
-
-        norm_factor = np.sum(det_accu[:, -1:])
-
-        if type(norm_factor) == torch.Tensor:
-            norm_factor = norm_factor.cpu().numpy()
-
-        det_accu_sum = np.zeros((1, 5))
-        det_accu_sum[:, 0:4] = np.sum(det_accu[:, 0:4], axis=0) / norm_factor
-        det_accu_sum[:, 4] = max_score
-        try:
-            dets = np.row_stack((dets, det_accu_sum))
-        except:
-            dets = det_accu_sum
-    dets = dets[0:750, :]
-    return dets
 
 
 def infer(net, img, transform, thresh, cuda, shrink):
@@ -129,43 +72,6 @@ def infer(net, img, transform, thresh, cuda, shrink):
     det = det[keep_index, :]
     return det
 
-
-def infer_flip(net, img, transform, thresh, cuda, shrink):
-    img = cv2.flip(img, 1)
-    det = infer(net, img, transform, thresh, cuda, shrink)
-    det_t = np.zeros(det.shape)
-    det_t[:, 0] = img.shape[1] - det[:, 2]
-    det_t[:, 1] = det[:, 1]
-    det_t[:, 2] = img.shape[1] - det[:, 0]
-    det_t[:, 3] = det[:, 3]
-    det_t[:, 4] = det[:, 4]
-    return det_t
-
-
-def infer_multi_scale_sfd(net, img, transform, thresh, cuda, max_im_shrink):
-    # shrink detecting and shrink only detect big face
-    st = 0.5 if max_im_shrink >= 0.75 else 0.5 * max_im_shrink
-    det_s = infer(net, img, transform, thresh, cuda, st)
-    index = np.where(np.maximum(det_s[:, 2] - det_s[:, 0] + 1, det_s[:, 3] - det_s[:, 1] + 1) > 30)[0]
-    det_s = det_s[index, :]
-    # enlarge one times
-    bt = min(2, max_im_shrink) if max_im_shrink > 1 else (st + max_im_shrink) / 2
-    det_b = infer(net, img, transform, thresh, cuda, bt)
-    # enlarge small iamge x times for small face
-    if max_im_shrink > 2:
-        bt *= 2
-        while bt < max_im_shrink:
-            det_b = np.row_stack((det_b, infer(net, img, transform, thresh, cuda, bt)))
-            bt *= 2
-        det_b = np.row_stack((det_b, infer(net, img, transform, thresh, cuda, max_im_shrink)))
-    # enlarge only detect small face
-    if bt > 1:
-        index = np.where(np.minimum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) < 100)[0]
-        det_b = det_b[index, :]
-    else:
-        index = np.where(np.maximum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) > 30)[0]
-        det_b = det_b[index, :]
-    return det_s, det_b
 
 def blur_faces(im,  dets , thresh=0.5, blur_level=100):
     """Blur detected bounding boxes."""
@@ -233,9 +139,6 @@ if __name__ == '__main__':
 
     shrink = 1
 
-    for i in range(4000):
-        cap.read()
-
     while cap.isOpened():
         if args.verbose > 0 and e_time is not None:
             ittime = (e_time - s_time) * (total_frame - f_count)
@@ -269,10 +172,6 @@ if __name__ == '__main__':
         e_time = time.time()
         f_count += 1
 
-        if f_count > 100:
-            break
-
     print("Finished!")
     cap.release()
     out.release()
-    
