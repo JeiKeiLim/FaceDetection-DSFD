73be6fe0f236e86aa5eb0e9ac4ab7b3e156518c6
Jongkuk Lim <lim.jeikei@gmail.com>
2019-12-31 11:50:22 +0900

    Added blur faces on video

--- blur_video.py
+++ blur_video.py
@@ -22,18 +22,29 @@ import math
 import matplotlib.pyplot as plt
 import time
 
+import warnings
+warnings.filterwarnings("ignore")
+
 plt.switch_backend('agg')
 
 parser = argparse.ArgumentParser(description='DSFD:Dual Shot Face Detector')
+parser.add_argument('file', type=str,
+                    help="Video file path")
+parser.add_argument('out', type=str,
+                    help='Output video path')
+parser.add_argument('--vertical', type=int, default=0,
+                    help='0 : horizontal video(default), 1 : vertical video')
+parser.add_argument('--verbose', type=int, default=0,
+                    help='Show current progress and remaining time')
+parser.add_argument('--reduce_scale', type=float, default=2,
+                    help='Reduce scale ratio. ex) 2 = half size of the input. Default : 2')
+
 parser.add_argument('--trained_model', default='weights/WIDERFace_DSFD_RES152.pth',
                     type=str, help='Trained state_dict file path to open')
-parser.add_argument('--save_folder', default='eval_tools/', type=str,
-                    help='Dir to save results')
-parser.add_argument('--visual_threshold', default=0.1, type=float,
+parser.add_argument('--threshold', default=0.1, type=float,
                     help='Final confidence threshold')
 parser.add_argument('--cuda', default=True, type=bool,
                     help='Use cuda to train model')
-parser.add_argument('--img_root', default='./data/worlds-largest-selfie.jpg', help='Location of test images directory')
 parser.add_argument('--widerface_root', default=WIDERFace_ROOT, help='Location of WIDERFACE root directory')
 args = parser.parse_args()
 
@@ -41,8 +52,8 @@ if args.cuda and torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 else:
     torch.set_default_tensor_type('torch.FloatTensor')
-if not os.path.exists(args.save_folder):
-    os.mkdir(args.save_folder)
+
+torch.set_grad_enabled(False)
 
 
 def bbox_vote(det):
@@ -72,8 +83,13 @@ def bbox_vote(det):
         if type(max_score) == torch.Tensor:
             max_score = max_score.cpu().numpy()
 
+        norm_factor = np.sum(det_accu[:, -1:])
+
+        if type(norm_factor) == torch.Tensor:
+            norm_factor = norm_factor.cpu().numpy()
+
         det_accu_sum = np.zeros((1, 5))
-        det_accu_sum[:, 0:4] = np.sum(det_accu[:, 0:4], axis=0) / np.sum(det_accu[:, -1:]).item()
+        det_accu_sum[:, 0:4] = np.sum(det_accu[:, 0:4], axis=0) / norm_factor
         det_accu_sum[:, 4] = max_score
         try:
             dets = np.row_stack((dets, det_accu_sum))
@@ -83,19 +99,6 @@ def bbox_vote(det):
     return dets
 
 
-def write_to_txt(f, det, event, im_name):
-    f.write('{:s}\n'.format(event + '/' + im_name))
-    f.write('{:d}\n'.format(det.shape[0]))
-    for i in range(det.shape[0]):
-        xmin = det[i][0]
-        ymin = det[i][1]
-        xmax = det[i][2]
-        ymax = det[i][3]
-        score = det[i][4]
-        f.write('{:.1f} {:.1f} {:.1f} {:.1f} {:.3f}\n'.
-                format(xmin, ymin, (xmax - xmin + 1), (ymax - ymin + 1), score))
-
-
 def infer(net, img, transform, thresh, cuda, shrink):
     if shrink != 1:
         img = cv2.resize(img, None, None, fx=shrink, fy=shrink, interpolation=cv2.INTER_LINEAR)
@@ -103,7 +106,6 @@ def infer(net, img, transform, thresh, cuda, shrink):
     x = Variable(x.unsqueeze(0), volatile=True)
     if cuda:
         x = x.cuda()
-    # print (shrink , x.shape)
     y = net(x)  # forward pass
     detections = y.data
     # scale each detection back up to the image
@@ -165,46 +167,63 @@ def infer_multi_scale_sfd(net, img, transform, thresh, cuda, max_im_shrink):
         det_b = det_b[index, :]
     return det_s, det_b
 
-
-def vis_detections(im, dets, image_name, thresh=0.5):
-    """Draw detected bounding boxes."""
+def blur_faces(im,  dets , thresh=0.5, blur_level=100):
+    """Blur detected bounding boxes."""
     class_name = 'face'
     inds = np.where(dets[:, -1] >= thresh)[0]
     if len(inds) == 0:
         return
-    print(len(inds))
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
+
     for i in inds:
         bbox = dets[i, :4]
         score = dets[i, -1]
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=2.5)
-        )
-        '''
-        ax.text(bbox[0], bbox[1] - 5,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=10, color='white')
-        '''
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                 fontsize=10)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.savefig(args.save_folder + image_name, dpi=fig.dpi)
+
+        top = max(int(bbox[1]), 0)
+        left = max(int(bbox[0]), 0)
+        bottom = min(int(bbox[3]), im.shape[0]-1)
+        right = min(int(bbox[2]), im.shape[1]-1)
+
+        im[top:bottom, left:right] = cv2.blur(im[top:bottom, left:right], (blur_level, blur_level))
 
 
-def test_oneimage():
-    torch.set_grad_enabled(False)
+def detect_faces(img, net, cfg=widerface_640):
+    # evaluation
+    cuda = args.cuda
+    transform = TestBaseTransform((104, 117, 123))
+    thresh = cfg['conf_thresh']
 
-    # load net
+    max_im_shrink = ((2000.0 * 2000.0) / (img.shape[0] * img.shape[1])) ** 0.5
+    shrink = max_im_shrink if max_im_shrink < 1 else 1
+
+    det = infer(net, img, transform, thresh, cuda, shrink)
+
+    return det
+
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(args.file)
+    frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    if args.vertical == 1:
+        out_size = (int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) // args.reduce_scale),
+                    int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) // args.reduce_scale))
+    else:
+        out_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) // args.reduce_scale),
+                    int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) // args.reduce_scale))
+
+    out = cv2.VideoWriter(args.out,
+                          fourcc,
+                          cap.get(cv2.CAP_PROP_FPS),
+                          out_size
+                          )
+    total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    f_count = 0
+    e_time = None
+    s_time = None
+
     cfg = widerface_640
+    # load net
     num_classes = len(WIDERFace_CLASSES) + 1  # +1 background
     net = build_ssd('test', cfg['min_dim'], num_classes)  # initialize SSD
     net.load_state_dict(torch.load(args.trained_model))
@@ -212,50 +231,48 @@ def test_oneimage():
     net.eval()
     print('Finished loading model!')
 
-    # evaluation
-    cuda = args.cuda
-    transform = TestBaseTransform((104, 117, 123))
-    thresh = cfg['conf_thresh']
-    # save_path = args.save_folder
-    # num_images = len(testset)
+    shrink = 1
 
-    # load data
-    path = args.img_root
-    img_id = 'face'
-    img = cv2.imread(path, cv2.IMREAD_COLOR)
+    for i in range(4000):
+        cap.read()
 
-    max_im_shrink = ((2000.0 * 2000.0) / (img.shape[0] * img.shape[1])) ** 0.5
-    shrink = max_im_shrink if max_im_shrink < 1 else 1
+    while cap.isOpened():
+        if args.verbose > 0 and e_time is not None:
+            ittime = (e_time - s_time) * (total_frame - f_count)
+            hour = int(ittime / 60.0 / 60.0)
+            minute = int((ittime / 60.0) - (hour * 60))
+            second = int(ittime % 60.0)
 
-    det0 = infer(net, img, transform, thresh, cuda, shrink)
-    det1 = infer_flip(net, img, transform, thresh, cuda, shrink)
-    # shrink detecting and shrink only detect big face
-    st = 0.5 if max_im_shrink >= 0.75 else 0.5 * max_im_shrink
-    det_s = infer(net, img, transform, thresh, cuda, st)
-    index = np.where(np.maximum(det_s[:, 2] - det_s[:, 0] + 1, det_s[:, 3] - det_s[:, 1] + 1) > 30)[0]
-    det_s = det_s[index, :]
-    # enlarge one times
-    factor = 2
-    bt = min(factor, max_im_shrink) if max_im_shrink > 1 else (st + max_im_shrink) / 2
-    det_b = infer(net, img, transform, thresh, cuda, bt)
-    # enlarge small iamge x times for small face
-    if max_im_shrink > factor:
-        bt *= factor
-        while bt < max_im_shrink:
-            det_b = np.row_stack((det_b, infer(net, img, transform, thresh, cuda, bt)))
-            bt *= factor
-        det_b = np.row_stack((det_b, infer(net, img, transform, thresh, cuda, max_im_shrink)))
-    # enlarge only detect small face
-    if bt > 1:
-        index = np.where(np.minimum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) < 100)[0]
-        det_b = det_b[index, :]
-    else:
-        index = np.where(np.maximum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) > 30)[0]
-        det_b = det_b[index, :]
-    det = np.row_stack((det0, det1, det_s, det_b))
-    det = bbox_vote(det)
-    vis_detections(img, det, img_id, args.visual_threshold)
+            print("Progress %d/%d(%.2f%%), Estimated time : %02d:%02d:%02d" %
+                  (f_count, total_frame, (f_count / total_frame) * 100, hour, minute, second))
 
+        s_time = time.time()
 
-if __name__ == '__main__':
-    test_oneimage()
+        ret, frame = cap.read()
+
+        if frame is None:
+            break
+
+        if args.vertical == 1:
+            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+        if args.reduce_scale == 1:
+            resized_img = frame
+        else:
+            resized_img = cv2.resize(frame, None, None, fx=args.reduce_scale, fy=args.reduce_scale)
+
+        det = detect_faces(resized_img, net)
+        blur_faces(resized_img, det, args.threshold)
+
+        out.write(resized_img)
+
+        e_time = time.time()
+        f_count += 1
+
+        if f_count > 100:
+            break
+
+    print("Finished!")
+    cap.release()
+    out.release()
+    
